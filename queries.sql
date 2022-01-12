38bc8d18b132f4178fde35cb19e5b68f8d7abcfb
Lauren McCall <lmmccolloch@gmail.com>
2022-01-11 19:36:53 -0500

    Beginning of Homework 12

--- queries.sql
+++ queries.sql
@@ -91,10 +91,52 @@ VALUES ('John Travolta', '300', 'scientologist', '6', 'm', 'false', '9');
 INSERT INTO "NewEmployees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime", "id")
 VALUES ('Oprah', '15000', 'president', '10', 'n', 'false', '11');
 
+CREATE TABLE "Departments"(
+  "Id" SERIAL PRIMARY KEY,
+  "DepartmentName" TEXT,
+  "Building" TEXT
+);
 
+CREATE TABLE IF NOT EXISTS "Employees"(
+ "Id" SERIAL PRIMARY KEY,
+"FullName" TEXT NOT NULL,
+"Salary" INT NOT NULL,
+"JobPosition" TEXT NOT NULL,
+"PhoneExtension" INT,
+"IsPartTime" BOOLEAN NOT NULL,
+"ParkingSpot" varchar(10)
+);
 
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime")
+VALUES ('Lazy Larry', '700', 'CEO', '1', 'e', 'true');
 
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime")
+VALUES ('Mary Poppins', '12000', 'cook', '43', 'f', 'false');
 
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime")
+VALUES ('Edgar Poe', '20', 'poet', '4', 'g', 'true');
 
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime")
+VALUES ('Martha Stewart', '7000', 'cook', '2', 'h', 'true');
 
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime")
+VALUES ('Maxine Waters', '13000', 'politician', '50', 'i', 'false');
 
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime")
+VALUES ('Daenerys Targaryen', '600', 'boss', '100', 'j', 'false');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime")
+VALUES ('Van Jones', '800', 'commentator', '89', 'k', 'true');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime")
+VALUES ('Jane Goodall', '650', 'primatologist', '91', 'l', 'true');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime")
+VALUES ('John Travolta', '300', 'scientologist', '6', 'm', 'false');
+
+INSERT INTO "Employees" ("FullName",  "Salary", "JobPosition", "PhoneExtension", "ParkingSpot", "IsPartTime")
+VALUES ('Oprah', '15000', 'president', '10', 'n', 'false');
+
+ALTER TABLE "Employees" ADD Column "DepartmentId" INTEGER NULL REFERENCES "Departments" ("Id");
+
+ALTER TABLE "Employees" DROP Column "ParkingSpot";
